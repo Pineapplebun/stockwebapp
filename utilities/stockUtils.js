74763c3c299c07567ba79209ab653d423810b4d7
pineapplebun <kevinjr.chen1@gmail.com>
2018-05-24 21:26:32 -0400

    actually fix the conflict

--- utilities/stockUtils.js
+++ utilities/stockUtils.js
@@ -24,13 +24,9 @@
             "5. volume": "24242019"
         },
 */
-<<<<<<< HEAD
 
 const https = require("https");
 
-=======
-const https = require("https");
->>>>>>> 080012b01772829defde493f0c3841b135027331
 module.exports = {
   getCollection: getCollection,
   getIntervalCollection: getIntervalCollection
