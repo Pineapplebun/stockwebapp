32d831a9600d806b7ee9b510537ba1fb3d92a0cf
pineapplebun <kevinjr.chen1@gmail.com>
2018-06-21 13:32:14 -0400

    refactor and add google oauth functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'),
+  passport = require('passport'),
+  GoogleStrategy = require('passport-google-oauth').OAuthStrategy,
+  router = express.Router(),
+  User = require('../db/models/user');
 
-router.post('/auth/:provider', (req, res) => {
-  // Check if id_token is valid
-  // if valid, 
-  // Check if the user is already registered => add to database, and/or retrieve details to store in session
-  // finally,
-  // set a session for the user
-  const sess = req.session;
-  sess.userid = ''; // will create the session in redis
-  
-  res
-  .status(200)
-  .json({});
+// Use the google oauth strategy when passport.authenticate is called
+passport.use(new GoogleStrategy({
+    consumerKey: process.env.GOOGLE_CONSUMER_KEY,
+    consumerSecret: process.env.GOOGLE_CONSUMER_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // Check if the user is already registered => add to database, and/or retrieve details to store in session
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  })
+);
 
-  // if not valid
-  // redirect to login page
-})
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => { // if valid
+    // finally,
+    // set a session for the user
+    const sess = req.session;
+    sess.userid = 'userid';
+    res.redirect('/');
+  }
+)
 
 module.exports = router;
