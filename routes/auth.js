881c2d72599e3e26407bdec586f325dccbdb123f
pineapplebun <kevinjr.chen1@gmail.com>
2018-06-22 00:30:36 -0400

    implement new user functionality:

--- routes/auth.js
+++ routes/auth.js
@@ -4,29 +4,53 @@ const express = require('express'),
   router = express.Router(),
   User = require('../db/models/user');
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+      done(err, user);
+  });
+});
+
 // Use the google oauth strategy when passport.authenticate is called
 passport.use(new GoogleStrategy({
-    consumerKey: process.env.GOOGLE_CONSUMER_KEY,
-    consumerSecret: process.env.GOOGLE_CONSUMER_SECRET,
-    callbackURL: '/auth/google/callback'
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
   },
   function(accessToken, refreshToken, profile, done) {
     // Check if the user is already registered => add to database, and/or retrieve details to store in session
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    User.findOne({ providerUserId: profile.id }, function (err, user) {
+      if (err) { return done(err, user); }
+      
+      // New user
+      if (!user) { // user false means not in db
+        const user = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          providerUserId: profile.id,
+          authProvider: 'google',
+          portfolios: [],
+        });
+
+        // save the user
+        user.save(function(err) {
+          if (err) { throw err; }
+        });
+      }
       return done(err, user);
+
     });
   })
 );
 
-router.get('/auth/google',
+router.get('/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => { // if valid
-    // finally,
-    // set a session for the user
-    const sess = req.session;
-    sess.userid = 'userid';
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/google/login' }),
+  (req, res) => {
     res.redirect('/');
 });
 
