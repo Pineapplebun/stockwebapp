8df048246e3cb7d18a4408233290cb8e854cada4
pineapplebun <kevinjr.chen1@gmail.com>
2018-06-30 18:22:29 -0400

    add unique to email and providerId fields

--- db/models/user.js
+++ db/models/user.js
@@ -6,11 +6,11 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    providerUserId: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    providerUserId: {type: String, required: true, unique: true},
     authProvider: {type: String, required: true},
     portfolios: [{type: Schema.ObjectId, ref: 'Portfolio', required: true}],
 });
 
 //Export function to create "SomeModel" model class
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('user', userSchema);
