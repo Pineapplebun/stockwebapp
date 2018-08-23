9107119ea04db017e7112a3c4c014d43fe32ab7f
pineapplebun <kevinjr.chen1@gmail.com>
2018-08-23 01:18:52 -0400

    decrease indent size to 2

--- routes/controllers/indexController.js
+++ routes/controllers/indexController.js
@@ -1,10 +1,10 @@
 module.exports = {
-    index,
+  index,
 }
 
 function index(req, res) {
-    res
-    .status(200)
-    .cookie('last_resource_request', '/', { maxAge: 600000 })
-    .sendFile('index.html', { root: path.join(__dirname, '..', 'build') });
+  res
+  .status(200)
+  .cookie('last_resource_request', '/', { maxAge: 600000 })
+  .sendFile('index.html', { root: path.join(__dirname, '..', 'build') });
 }
